f7a86fa46331fcaa715e77b4773f21fe7eb9b4c3
danish vahajuddin ansari <danishvahajuddinansari@danishs-MacBook-Air.local>
2025-03-14 16:20:29 +0530

    git init

---
+++ danish/practice.js/14m.js
@@ -0,0 +1,28 @@
+const accountpass=1234
+let emailid= "abs@gmail.com"
+
+var accountcity="jaipur"
+console.table([accountpass,emailid,accountcity])
+ 
+
+var studsname= "danish"
+const studid=65
+let  studemail="ab@gmail.com"
+let student;
+console.log([studsname,studid,studemail]);
+
+/* number = 123
+   bigint
+   string=" "
+   boolean => false
+   null => 
+    undefined
+   symbol
+   object
+   */
+
+console.log(typeof(studid))
+console.log(typeof(null))  // prints object
+console.log(undefined)  // prints undefined
+
+
